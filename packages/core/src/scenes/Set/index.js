5db590b73ab6858d907948a91fb108ac5b61df9d
David Hérault <dherault@gmail.com>
2020-05-20 12:17:16 +0200

    About to eject the web app

--- packages/core/src/scenes/Set/index.js
+++ packages/core/src/scenes/Set/index.js
@@ -1,19 +1,33 @@
 import React from 'react'
-import * as eva from '@eva-design/eva'
+import { View, Image } from 'react-native'
 import { Layout, Text } from '@ui-kitten/components'
 
 import useCommonStyles from '../../commonStyles'
 
 import sets from '../../../data/sets.json'
+import cards from '../../../data/cards.json'
 
-function SetScene(props) {
+function SetScene({ route }) {
   const _ = useCommonStyles()
+
+  const { setId } = route.params;
+  const set = sets.find(set => set.id === setId)
 
   return (
     <Layout style={_.sceneRoot}>
       <Text>
-        {JSON.stringify(props)}
+        {set.name}
       </Text>
+      {cards
+      .filter(card => card.setId === setId)
+      .map(card => (
+        <View key={card.number}>
+          <Text>
+            {card.name}
+          </Text>
+          <Image source={{ uri: `https://pokedextcg.com/images/${card.imageFileName}` }} />
+        </View>
+      ))}
     </Layout>
   )
 }
